920b1f2cb843dbf7459f516632a5519d20ed1579
sanju desai <sanjudesai2929@gmail.com>
2022-07-25 11:43:21 +0530

    first commit

---
+++ Model/user.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema=new mongoose.Schema({
+    user_id:{
+        type:"string"
+    }
+})
+
+const user = new  mongoose.model("user",Schema)
+module.exports= user
---
+++ Server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require("http")
+const app = express();
+const cors = require('cors');
+const env = require("dotenv");
+const user = require("../Model/user.model")
+env.config()
+const port = process.env.PORT
+var server = http.createServer(app)
+require("../db/db.js")
+var io = require('socket.io')(server, {
+
+})
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+io.on("connection", async (socket) => {
+    console.log("connected");
+    console.log(socket)
+        const data = await user.insertMany({ user_id: socket.id })
+})
+server.listen(port, () => {
+    console.log("server started");
+    // io.emit("connection",{id:"aaa4556asg"})
+})
+
