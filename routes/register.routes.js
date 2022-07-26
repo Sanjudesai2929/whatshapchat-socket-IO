e7e04da57a415603fb2e0657d9fa82a71c29b240
sanju desai <sanjudesai2929@gmail.com>
2022-07-26 17:53:28 +0530

    change

--- routes/register.routes.js
+++ routes/register.routes.js
@@ -29,10 +29,10 @@ router.post("/register", async (req, res) => {
         })
     }
 })
-router.get("/",({req,res})=>{
-    res.json({
-   status:true,
-   message:"connected..."
-    })
-})
+// router.get("/",({req,res})=>{
+//     res.json({
+//    status:true,
+//    message:"connected..."
+//     })
+// })
 module.exports = router
