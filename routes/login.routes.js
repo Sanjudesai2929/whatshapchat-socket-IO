ff89b3515bce180ebfdb626233c571f232ae5c18
sanju desai <sanjudesai2929@gmail.com>
2022-07-27 11:35:34 +0530

    change

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,35 +1,35 @@
-const register = require("../Model/register.model")
+const Register = require("../Model/register.model")
 const express = require("express")
 const bcrypt = require("bcrypt")
 const router = require("./register.routes")
 const loginRouter = new express.Router()
 
-router.post("/login",async(req,res)=>{
-    const {username,password}=req.body
-  
-        const userData =await register.findOne({username})
-        console.log(userData);
-        if(userData){
-               const cofirm = await bcrypt.compare(password,userData.password)
-               if(cofirm){
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body
+console.log(username);
+    var userData = await Register.findOne({ username :username })
+    console.log(userData);
+    if (userData) {
+        const cofirm = await bcrypt.compare(password, userData.password)
+        if (cofirm) {
             res.json({
-                status:true,
-                message:"Logging Successfully"
-            })
-               }
-               else{
-                res.json({
-                    status:false,
-                    message:"Invaild username and password ! Try again"
-                })
-               }
-        }
-        else{
-            res.json({
-                status:false,
-                message:"Username is not Exist"
+                status: true,
+                message: "Logging Successfully"
             })
         }
+        else {
+            res.json({
+                status: false,
+                message: "Invaild username and password ! Try again"
+            })
+        }
+    }
+    else {
+        res.json({
+            status: false,
+            message: "Username is not Exist"
+        })
+    }
 
 })
-module.exports =loginRouter
+module.exports = loginRouter
