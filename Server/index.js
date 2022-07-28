ba9760b94d809506932f7ba10a6e89a4ce569a74
sanju desai <sanjudesai2929@gmail.com>
2022-07-28 11:47:41 +0530

    change

--- Server/index.js
+++ Server/index.js
@@ -27,14 +27,14 @@ io.on("connection", async (client) => {
     // console.log(client);
     console.log("connected");
 
-    client.on('connected-user', async (connect) => {
+    client.on('connected-user',  (connect) => {
         console.log("connected user is ",connect);
-        client.on("current-user",(data )=>{
+        client.on("current-user",async(data )=>{
             current = data
-            console.log("current user is",current);
+            const viewMsg = await Message.find({ $and :[{targetId: connect ,sentBy:data}]})
+            console.log("current user is",data);
+            io.emit('connected-user', viewMsg);
         })
-        const viewMsg = await Message.find({ $and :[{targetId: connect ,sentBy:current}]})
-        io.emit('connected-user', viewMsg);
     });
     const data = await user.insertMany({ user_id: client.id })
     connectedUser.add(client.id);   
