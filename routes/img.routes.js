2361c6b70547f2541940db4d689d3dd252dae2a7
sanju desai <sanjudesai2929@gmail.com>
2022-08-02 11:53:49 +0530

    change

--- routes/img.routes.js
+++ routes/img.routes.js
@@ -2,16 +2,18 @@ const mongoose = require('mongoose');
 const express = require('express');
 const upload = require("../multer/multer.utils")
 const router = new express.Router();
+const fs =require("fs")
 const env = require("dotenv")
 env.config()
 router.post("/addimg", upload.single("file"), (req, res) => {
     const file = req.file.filename
-    const encoded = req.file.filename.buffer.toString('base64')
-    console.log(encoded);
+    // const encoded = req.file.path.buffer.toString('base64')
+    const data =new Buffer(req.file.path).toString("base64")
+    console.log(data );
     res.json({
         status: true,
         message: "Image upload successfully",
-        file: process.env.BASE_URL +"/upload/"+ file
+        file: process.env.BASE_URL +"/upload/"+ data
     })
 })
 
