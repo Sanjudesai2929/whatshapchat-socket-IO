8ca5b09364e4fd67d46b1687b53f3b5b9fd6352d
sanju desai <sanjudesai2929@gmail.com>
2022-08-02 11:03:51 +0530

    change

--- routes/img.routes.js
+++ routes/img.routes.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const upload =require("../multer/multer.utils")
-const router= new express.Router();
-
-router.post("/addimg",upload.single("file"),(req,res)=>{
-    const file =req.file
-    console.log(file);
+const upload = require("../multer/multer.utils")
+const router = new express.Router();
+const env = require("dotenv")
+env.config()
+router.post("/addimg", upload.single("file"), (req, res) => {
+    const file = req.file.filename
+    res.json({
+        status: true,
+        message: "Image upload successfully",
+        file: process.env.BASE_URL + file
+    })
 })
 
-module.exports =router
+module.exports = router
