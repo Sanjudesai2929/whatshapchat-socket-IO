61ea9c0476dd6a846a3916c8ea0c0e53002cfb70
sanju desai <sanjudesai2929@gmail.com>
2022-08-04 11:01:34 +0530

    change

--- routes/img.routes.js
+++ routes/img.routes.js
@@ -32,7 +32,7 @@ router.post("/addimg" ,(req, res) => {
 })
 router.get('/', function (req, res) {
     res.json({
-        message:"hello3"
+        message:"hello4"
     })
 })
 module.exports = router
