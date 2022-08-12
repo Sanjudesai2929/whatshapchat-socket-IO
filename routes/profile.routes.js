33b99d7beb975b5c3f35ca6351a0b34eea5112f8
sanju desai <sanjudesai2929@gmail.com>
2022-08-12 18:21:45 +0530

    change

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -3,13 +3,13 @@ const express = require("express")
 const router = new express.Router()
 router.post("/profileUpdate/:id", async (req, res) => {
     const _id =req.params.id
-    const { username, country_code, email, phone,bio } = req.body
+    const { username, country_code, email, phone,bio,password } = req.body
     try {
-        const user= await register.updateOne({_id:_id},{$set:{username, country_code, email, phone ,bio}} )
+        const user= await register.updateOne({_id:_id},{$set:{username, country_code, email, phone ,bio,password}} )
         if(user){
             res.json({
                 status:true,
-                message:"update Successfully ",
+                message:"update Successfully",
             })
         }
     }
@@ -20,8 +20,6 @@ router.post("/profileUpdate/:id", async (req, res) => {
             })  
     }
 })
-
-
 module.exports = router
 
 
