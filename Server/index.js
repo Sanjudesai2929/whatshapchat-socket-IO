244b944a35bd34d5c3ba0bcb80e56c7fa7c7ebe1
sanju desai <sanjudesai2929@gmail.com>
2022-08-22 11:19:49 +0530

    change

--- Server/index.js
+++ Server/index.js
@@ -302,13 +302,11 @@ io.on("connection", async (client) => {
         client.emit("deliver-status", { msgid: user.msgid, msgstatus: true })
         await GroupMsg.updateOne({ msgid: user.msgid }, { $set: { messagestatus: "send" } })
         const msg_data = {
-            _id:msg[0]._id,
+            _id:msg[0].grpid,
             message: msg[0].message,
             sentByUsername: msg[0].sentByUsername,
-        
             time: msg[0].time
         }
-
         console.log("msg_data", msg_data);
         client.emit("user-data-list-update", msg_data)
         client.broadcast.emit("user-data-list-update", msg_data)
