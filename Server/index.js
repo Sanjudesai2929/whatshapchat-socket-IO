8c7ddcfd7fc10221344f1af4bd86f22a57aa3d11
sanju desai <sanjudesai2929@gmail.com>
2022-08-24 12:54:57 +0530

    change

--- Server/index.js
+++ Server/index.js
@@ -133,7 +133,7 @@ io.on("connection", async (client) => {
     client.on('connected-user', async (data) => {
         console.log("connected user is ", data);
         client.broadcast.emit('is_online', '🔵 <i>' + data.current_user + ' join the chat..</i>');
-        const connectMsg = await Message.find({ $or: [{ $and: [{ targetId: data.targetId, sentById: data.sentById }] }, { $and: [{ targetId: data.sentById, sentById: data.targetId }] }] }).limit(100).sort({ date: 1 }).select({ "dateTime": 0 })
+        const connectMsg = await Message.find({ $or: [{ $and: [{ targetId: data.targetId, sentById: data.sentById }] }, { $and: [{ targetId: data.sentById, sentById: data.targetId }] }] }).limit(500).sort({ date: 1 }).select({ "dateTime": 0 })
         console.log("connectMsg", connectMsg);
         client.emit('connected-user', connectMsg);
     });
