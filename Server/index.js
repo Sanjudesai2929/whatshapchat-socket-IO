3a90ff0023bef3af504e7aaaddc7210d1803bf7f
sanju desai <sanjudesai2929@gmail.com>
2022-08-27 12:41:48 +0530

    change

--- Server/index.js
+++ Server/index.js
@@ -50,7 +50,7 @@ io.on("connection", async (client) => {
         client.emit("user-online-status-update", { status: "online" })
         await Register.update({ _id: connectedId }, { $set: { status: "online" } })
         const user = await Register.find({ _id: connectedId })
-        client.emit("loginid",user[0].deviceid)
+        client.emit("deviceVerification",user[0].deviceid)
         // client.broadcast.emit('is_online', '🔵 <i>' + user[0].username + ' join the chat..</i>');
         console.log(user);
         //Get the user list data
