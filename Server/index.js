332db8b2367987d32c3814ae9e822dbda1da4ad3
sanju desai <sanjudesai2929@gmail.com>
2022-09-07 11:38:44 +0530

    chnage

--- Server/index.js
+++ Server/index.js
@@ -188,7 +188,7 @@ io.on(process.env.CONNECTION, async (client) => {
             console.log("val3", val3);
             io.sockets.emit("user-data-list-update", val2)
             // client.broadcast.emit("user-data-list-update", val3)
-            io.broadcast.to(targetSocketId[0].socketId).emit("user-data-list-update", val3)
+            client.broadcast.to(targetSocketId[0].socketId).emit("user-data-list-update", val3)
             // socketIds[msgData[0].targetId].emit("user-data-list-update", val3)
         }
         else {
