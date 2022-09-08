c6dd1596e13c9b29f7a384105a2fc80a99061e63
sanju desai <sanjudesai2929@gmail.com>
2022-09-08 16:28:31 +0530

    chnage

--- Server/index.js
+++ Server/index.js
@@ -161,10 +161,10 @@ io.on(process.env.CONNECTION, async (client) => {
         })
         console.log("msgData", msgData)
         const targetSocketId = await Register.find({ _id: data.targetId })
-        // io.to(targetSocketId[0].socketId).emit(process.env.MESSAGE_RECEIVE, msgData)
+        io.in(targetSocketId[0].socketId).emit(process.env.MESSAGE_RECEIVE, msgData)
         // client.broadcast.emit(process.env.MESSAGE_RECEIVE, msgData)
 
-        socketIds[data.targetId].emit(process.env.MESSAGE_RECEIVE, msgData)
+        // socketIds[data.targetId].emit(process.env.MESSAGE_RECEIVE, msgData)
         // io.sockets.emit(process.env.MESSAGE_RECEIVE, msgData)
         if (msgData) {
             client.emit(process.env.DELIEVER_STATUS, { msgid: data.msgid, msgstatus: true })
