2e43269bb3e92c88003b272d4e5ae0de2afeba89
sanju desai <sanjudesai2929@gmail.com>
2022-09-08 15:49:48 +0530

    chnage

--- Server/index.js
+++ Server/index.js
@@ -308,7 +308,7 @@ io.on(process.env.CONNECTION, async (client) => {
         console.log("grp message receive", msg);
         // client.broadcast.emit(process.env.GRP_MESSAGE_RECEIVE, msg)
 
-        client.broadcast.to(groupmsga[0].groupName).emit(process.env.GRP_MESSAGE_RECEIVE, msg)
+        client.to(groupmsga[0].groupName).emit(process.env.GRP_MESSAGE_RECEIVE, msg)
         // client.broadcast.emit(process.env.GRP_MESSAGE_RECEIVE, msg)
 
         client.emit(process.env.DELIEVER_STATUS, { msgid: user.msgid, msgstatus: true })
