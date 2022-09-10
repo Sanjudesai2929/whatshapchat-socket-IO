b6bce69fabfd60d6a0cd23328864cc7358f04d3d
sanju desai <sanjudesai2929@gmail.com>
2022-09-10 14:02:37 +0530

    change

--- Server/index.js
+++ Server/index.js
@@ -164,7 +164,7 @@ io.on(process.env.CONNECTION, async (client) => {
         console.log("msgData", msgData)
         const targetSocketId = await Register.find({ _id: data.targetId })
         // client.to(targetSocketId[0].socketId).emit(process.env.MESSAGE_RECEIVE, msgData)
-        client.in(data.targetId).emit(process.env.MESSAGE_RECEIVE, msgData)
+        io.in(data.targetId).emit(process.env.MESSAGE_RECEIVE, msgData)
         // client.broadcast.emit(process.env.MESSAGE_RECEIVE, msgData)
         // console.log(socketIds[data.targetId]);
         console.log(socketIds);
