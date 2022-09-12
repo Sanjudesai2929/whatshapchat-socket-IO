f9e23c9c7bcc47718ef84998464b34c17c99689c
sanju desai <sanjudesai2929@gmail.com>
2022-09-12 16:32:19 +0530

    change

--- Server/index.js
+++ Server/index.js
@@ -322,7 +322,7 @@ io.on(process.env.CONNECTION, async (client) => {
 
         arrayData.map((data) => {
             console.log(data.socketId);
-            io.broadcast.connected[data.socketId].emit(process.env.GRP_MESSAGE_RECEIVE, msg)
+            client.broadcast.connected[data.socketId].emit(process.env.GRP_MESSAGE_RECEIVE, msg)
             // client.to(data.socketId).emit(process.env.GRP_MESSAGE_RECEIVE, msg)
         })
         // io.sockets.to(user.grpid).emit(process.env.GRP_MESSAGE_RECEIVE, msg)
